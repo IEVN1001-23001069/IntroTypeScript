d8d8e719a4f68aa1ca1cf79e4260e24f758650e9
IEVN1001-23001069 <lunasolisort+escolar@gmail.com>
2026-09-05 10:00:12 -0600

    Operaciones Basicas 05/09/2026 9:59 a.m.

---
+++ src/temasTS/01-tipos-bas.ts
@@ -0,0 +1,17 @@
+console.log("Hola Mundo")
+
+var num1=23 //Esta es desuso pero se puede utilizar
+let num2=25 //Local, normalmente utilizaremos let
+const num3=22 //constantes globales
+ //Por defecto el tipo de dato se va tomar por el primer tipo de valor que se asigne
+ //En este caso entero
+
+ let nombres:string
+ let num:number
+ let activo:boolean
+ //Se recomienda hacer la declaración de la variable previamente pero no es necesario en todos los casos
+ //Se puede definir la variable dentro de las opreaciones
+
+ let suma:number=num1+num2+num3
+
+ console.log('La suma es: '+ suma)
